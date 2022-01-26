aa04fa66b43b173798b6c1f20d0b5a36d32b70b4
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-01-27 00:18:16 +0530

    created project and added simple rest controller

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Gender;
+import com.example.demo.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        List<Student> students = Arrays.asList(
+                new Student(1L, "Jamila", "jamila@amigoscode.com", Gender.FEMALE),
+                new Student(2L, "John Doe", "joh@gmail.com", Gender.MALE)
+        );
+        return students;
+    }
+}
