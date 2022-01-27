e0ab638d2efdce03e22bf7b7d29170f92322c109
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-01-27 14:13:51 +0530

    Installed antd design and some changes in files

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,11 @@
+import './app.css';
+import {DatePicker} from 'antd';
+
 function App() {
     return (
         <div>
             <h1>Hello React App</h1>
+            <DatePicker/>
         </div>
     );
 }
